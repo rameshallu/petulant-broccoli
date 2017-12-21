c674a71827eac6c633043bb9cdf900bb1c34503d
Ramesh Allu <ramesh.allu@gmail.com>
2017-12-21 16:08:30 +0530

    Base project structure.

---
+++ samples/sample-spring-boot-web/src/main/java/com/rameshallu/samples/SampleSpringBootWebApplication.java
@@ -0,0 +1,12 @@
+package com.rameshallu.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringBootWebApplication.class, args);
+	}
+}
